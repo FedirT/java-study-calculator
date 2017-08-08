1383e3c0c459fc14e3172e7b063a3a222b54c42f
Fedya <tiutiunnik.f@gmail.com>
2017-08-08 21:23:50 +0300

    v_0.1 (after refactoring)

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,6 +1,3 @@
-/**
- * Created by 1 on 02.08.2017.
- */
 import java.util.Scanner;
 
 class Calculator {
@@ -11,59 +8,60 @@ class Calculator {
 
     private double a[]; //massive of operands
 
-    /*Calculator() {
+    private char sel; //operation (plus or minus/'1' or '2')
+
+    private double result;
+
+    /*//Overloading constructors
+    Calculator(int num, int arr[]) {
 
         n = num;
 
-        a = arr;
+        int a[] = arr;
+
+    }
+
+    Calculator(int num, double arr[]) {
+
+        n = num;
+
+       double a[] = arr;
 
     }*/
 
     //enter number of operands and initialize them
     double[] enterOp() {
-
-        int num;
-
-        double arr[];
-
         System.out.println("Enter the number of operands");
-        num = in.nextInt();
-        arr = new double[num];
-            for (int i=0; i<arr.length; i++) {
+        n = in.nextInt();
+        a = new double[n];
+            for (int i=0; i<a.length; i++) {
                 System.out.println("Enter operand No." + (i+1));
-                arr[i] = in.nextDouble();
+                a[i] = in.nextDouble();
             }
-        return arr;
+        return a;
         }
 
     //sum of operands
-    double add(double arr[]) {
-
-        double result = 0;
-
-        for (int i=arr.length-1; i>=0; i--){
-            result += arr[i];
+    private double add() {
+        result = 0;
+        for (int i=a.length-1; i>=0; i--){
+            result += a[i];
         }
         return result;
     }
 
     //subtraction of operands
-    double subtr(double arr[]) {
-
-        double result = arr[0];
-
-        for (int i=1; i<arr.length; i++){
-            result -= arr[i];
+    private double subtr() {
+        result = a[0];
+        for (int i=1; i<a.length; i++){
+            result -= a[i];
         }
         return result;
     }
 
     //chose of operation (plus or minus)
-    char choseSign()
+    private char choseSign()
     throws java.io.IOException {
-
-        char sel;
-
         do {
             System.out.println("1. addition");
             System.out.println("2. subtraction");
@@ -74,26 +72,19 @@ class Calculator {
     }
 
     //start calculator
-    void start()
+    public void start()
     throws java.io.IOException {
-
-        char choise;
-
-        double res;
-
-        double arr[] = this.enterOp();
-
-        choise = this.choseSign();
-        if (choise=='1') {
-            res = add(arr);
-            System.out.println("Result: " + res);
+        a = enterOp();
+        sel = choseSign();
+        if (sel=='1') {
+            result = add();
+            System.out.println("Result: " + result);
         } else
         {
-            res = subtr(arr);
-            System.out.println("Result: " + res);
+            result = subtr();
+            System.out.println("Result: " + result);
         }
     }
-
 }
 
 class CalculatorTest {
