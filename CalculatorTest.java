aab1545945c254fb3163e969976168bfc51d4f5b
Fedya <tiutiunnik.f@gmail.com>
2017-08-23 09:58:34 +0300

    Delete enterOp() method, all instructions move to Calculator() Delete class CalculatorTest, method main() move to Calculator Change start() method

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -8,38 +8,36 @@ class Calculator {
 
     private double a[]; //massive of operands
 
-    private char sel; //operation (plus or minus/'1' or '2')
+    private int sel; //operation (plus or minus/'1' or '2')
 
     private double result;
 
-    /*//Overloading constructors
-    Calculator(int num, int arr[]) {
-
-        n = num;
-
-        int a[] = arr;
-
+    //Overloading constructors
+    Calculator() {
+        //enter number of operands and initialize them
+        System.out.println("Enter the number of operands");
+        n = in.nextInt();
+        a = new double[n];
+        for (int i=0; i<a.length; i++) {
+            System.out.println("Enter operand No." + (i+1));
+            a[i] = in.nextDouble();
+        }
+        //chose of operation (plus or minus)
+        do {
+            System.out.println("1. addition");
+            System.out.println("2. subtraction");
+            System.out.println("Select the item");
+            sel = in.nextInt();
+        } while (sel<1 || sel>2);
     }
 
     Calculator(int num, double arr[]) {
 
         n = num;
 
-       double a[] = arr;
+        double a[] = arr;
 
-    }*/
-
-    //enter number of operands and initialize them
-    double[] enterOp() {
-        System.out.println("Enter the number of operands");
-        n = in.nextInt();
-        a = new double[n];
-            for (int i=0; i<a.length; i++) {
-                System.out.println("Enter operand No." + (i+1));
-                a[i] = in.nextDouble();
-            }
-        return a;
-        }
+    }
 
     //sum of operands
     private double add() {
@@ -59,24 +57,11 @@ class Calculator {
         return result;
     }
 
-    //chose of operation (plus or minus)
-    private char choseSign()
-    throws java.io.IOException {
-        do {
-            System.out.println("1. addition");
-            System.out.println("2. subtraction");
-            System.out.println("Select the item");
-            sel = (char) System.in.read();
-        } while (sel<'1' || sel>'2');
-        return sel;
-    }
-
     //start calculator
+    //it may be a static method
     public void start()
     throws java.io.IOException {
-        a = enterOp();
-        sel = choseSign();
-        if (sel=='1') {
+        if (sel==1) {
             result = add();
             System.out.println("Result: " + result);
         } else
@@ -85,12 +70,22 @@ class Calculator {
             System.out.println("Result: " + result);
         }
     }
-}
-
-class CalculatorTest {
     public static void main(String args[])
         throws java.io.IOException {
+
+        /*
+        System.out.println("Enter number of operands")
+        Scanner in = new Scanner(System.in);
+        num = in.nextInt();
+        ...
+        System.out.println("Enter operands")
+        arr[i] = in.nextInt();
+        ...
+        Calculator ob = new Calculator(num, arr[i])
+        */
+
         Calculator ob = new Calculator();
         ob.start();
+
     }
 }
