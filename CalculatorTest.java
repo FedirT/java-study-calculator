ba90a71d7b037559457e36cb9242b9b9038ae088
Fedya <tiutiunnik.f@gmail.com>
2017-09-16 22:20:54 +0300

    Refactoring of start() method Add createCalc() metod Add exception handling

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Calculator {
@@ -6,50 +7,67 @@ class Calculator {
 
     private int n; //number of operands
 
-    private double a[]; //massive of operands
+    private float a[]; //massive of operands
 
     private int sel; //operation (plus or minus/'1' or '2')
 
-    private double result;
+    private float result;
 
     //Overloading constructors
-    Calculator() {
-        //enter number of operands and initialize them
-        System.out.println("Enter the number of operands");
-        n = in.nextInt();
-        a = new double[n];
-        for (int i=0; i<a.length; i++) {
-            System.out.println("Enter operand No." + (i+1));
-            a[i] = in.nextDouble();
+    Calculator()
+        throws java.io.IOException {
+        try {
+            //enter number of operands and initialize them
+            System.out.println("Enter the number of operands");
+            n = in.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println(e);
+            System.out.println("You has a mistake (entering number of operands), please try again");
+            createCalc();
+        }
+        try {
+            a = new float[n];
+            for (int i = 0; i < a.length; i++) {
+                System.out.println("Enter operand No." + (i + 1));
+                a[i] = in.nextFloat();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(e);
+            System.out.println("You has a mistake (entering operands), please try again");
+            createCalc();
         }
         //chose of operation (plus or minus)
-        do {
-            System.out.println("1. addition");
-            System.out.println("2. subtraction");
-            System.out.println("Select the item");
+        System.out.println("1. addition");
+        System.out.println("2. subtraction");
+        System.out.println("Select the item");
+        try {
             sel = in.nextInt();
-        } while (sel<1 || sel>2);
+        } catch (InputMismatchException e) {
+            System.out.println(e);
+            System.out.println("Wrong select of item, please try again");
+            createCalc();
+        }
     }
 
-    Calculator(int num, double arr[]) {
+    /*Calculator(int num, float arr[]) {
 
         n = num;
 
-        double a[] = arr;
+        float a[] = arr;
 
-    }
+    }*/
 
     //sum of operands
-    private double add() {
+    private float add() {
         result = 0;
-        for (int i=a.length-1; i>=0; i--){
-            result += a[i];
+        for (float i : a){
+            result += i;
         }
         return result;
     }
 
     //subtraction of operands
-    private double subtr() {
+    private float subtr() {
         result = a[0];
         for (int i=1; i<a.length; i++){
             result -= a[i];
@@ -57,18 +75,37 @@ class Calculator {
         return result;
     }
 
+    //create object of Calculator
+    static void createCalc()
+    throws java.io.IOException {
+        Calculator ob = new Calculator();
+        ob.start();
+    }
+
     //start calculator
     //it may be a static method
     public void start()
     throws java.io.IOException {
-        if (sel==1) {
-            result = add();
-            System.out.println("Result: " + result);
-        } else
-        {
-            result = subtr();
-            System.out.println("Result: " + result);
+        try {
+            switch (sel) {
+                case 1:
+                    result = add();
+                    System.out.println("Result: " + result);
+                    break;
+                case 2:
+                    result = subtr();
+                    System.out.println("Result: " + result);
+                    break;
+                default:
+                    System.out.println("Wrong select of item, please try again");
+                    createCalc();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(e);
+            System.out.println("Wrong select of item, please try again");
+            createCalc();
         }
+
     }
     public static void main(String args[])
         throws java.io.IOException {
@@ -84,8 +121,13 @@ class Calculator {
         Calculator ob = new Calculator(num, arr[i])
         */
 
-        Calculator ob = new Calculator();
-        ob.start();
+        try {
+            Calculator.createCalc();
+        } catch (InputMismatchException e) {
+            System.out.println(e);
+            System.out.println("Wrong select of item, please try again");
+            createCalc();
+        }
 
     }
 }
